7958509991fdbd4e17462bed84caa332fc553521
user1975 <jisim.kim@hanmail.net>
2017-07-24 16:09:38 +0900

    Add files via upload

---
+++ ContextMenu_wXML_MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.kjs.ex07_02;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    LinearLayout baseLayout;
+    Button button1,button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        baseLayout = (LinearLayout) findViewById(R.id.baseLayout);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+
+        setTitle("배경색 바꾸기(컨텍스트 메뉴)");
+
+        registerForContextMenu(button1);
+        registerForContextMenu(button2);
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater mInflater = getMenuInflater();
+
+        if (v==button1) {
+            menu.setHeaderTitle("배경색 변경");
+            menu.setHeaderIcon(android.R.drawable.sym_def_app_icon);
+
+            mInflater.inflate(R.menu.menu1,menu);
+        }
+        if (v==button2) {
+            menu.setHeaderTitle("버튼 변경");
+            mInflater.inflate(R.menu.menu2,menu);
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itemRed:
+                baseLayout.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.itemGreen:
+                baseLayout.setBackgroundColor(Color.GREEN);
+                return true;
+            case R.id.itemBlue:
+                baseLayout.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.subRotate:
+                button1.setRotation(45);
+                return true;
+            case R.id.subSize:
+                button1.setScaleX(2);
+                return true;
+        }
+        return false;
+    }
+}
