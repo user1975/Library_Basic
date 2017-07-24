295b6960e6c82875a887a159d344582e2c61b2c2
user1975 <jisim.kim@hanmail.net>
2017-07-24 16:08:07 +0900

    Add files via upload

---
+++ OptionMenu_MainActivity_사진_각도변경.java
@@ -0,0 +1,51 @@
+package com.example.kjs.test07_01;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtAngle;
+    ImageView imageView1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("제주도 풍경");
+
+        edtAngle = (EditText) findViewById(R.id.edtAngle);
+        imageView1 = (ImageView) findViewById(R.id.imageView1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater mInflater = getMenuInflater();
+        mInflater.inflate(R.menu.menu1, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itemRotate:
+                imageView1.setRotation(Float.parseFloat(edtAngle.getText().toString()));
+                return true;
+            case R.id.item1:
+                imageView1.setImageResource(R.drawable.jeju02);
+                return true;
+            case R.id.item2:
+                imageView1.setImageResource(R.drawable.jeju14);
+                return true;
+            case R.id.item3:
+                imageView1.setImageResource(R.drawable.jeju6);
+                return true;
+        }
+        return false;
+    }
+}
