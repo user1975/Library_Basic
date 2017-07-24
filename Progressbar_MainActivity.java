33b8627fb271e061ec44e0be76041c6c16493a01
user1975 <jisim.kim@hanmail.net>
2017-07-24 16:14:35 +0900

    Add files via upload

---
+++ Progressbar_MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.kjs.myprogress;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    ProgressDialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog = new ProgressDialog(MainActivity.this);
+                dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                dialog.setMessage("데이터를 확인하는 중입니다.");
+
+                dialog.show();
+            }
+        });
+    }
+}
